49bb2601dfc5d87734a08f2d2dfc5f91860c05d5
UwUssimo Robinson <uwussimo@icloud.com>
2022-11-16 19:01:04 +0500

    [FIX] fixed path dependence reliance

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,3 +1,5 @@
+// @ts-ignore
+import process from 'node:process'
 import { viteBundler } from '@vuepress/bundler-vite'
 import { webpackBundler } from '@vuepress/bundler-webpack'
 import { defineUserConfig } from '@vuepress/cli'
